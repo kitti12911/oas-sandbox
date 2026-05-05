80e837fba858a09206aa32cce60590263cc4fa26
fmt.Println("K") <50092335+kitti12911@users.noreply.github.com>
2026-05-05 15:29:07 +0700

    Feat/remove mock api (#10)
    * feat: test breaking change
    
    * fix: fix openapi pipeline not error

--- internal/api/users/v1/api.go
+++ internal/api/users/v1/api.go
@@ -96,14 +96,3 @@ func Register(h huma.API, deps api.Deps) {
 		return humautil.AffectedRows(resp.GetAffectedRows()), nil
 	}, humautil.WithTag(api.TagUsers))
 }
-
-func RegisterV2(h huma.API, deps api.Deps) {
-	huma.Get(h, "/users/mock", func(ctx context.Context, input *struct{}) (*MockUserOutput, error) {
-		return &MockUserOutput{
-			Body: MockUser{
-				ID:       "mock-user-v2",
-				Username: "mock-v2",
-			},
-		}, nil
-	}, humautil.WithTag(api.TagUsers))
-}
