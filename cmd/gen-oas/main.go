e018328625594ed48777d2ab8d9357f687975c9f
fmt.Println("K") <50092335+kitti12911@users.noreply.github.com>
2026-05-11 01:11:57 +0700

    Feat/add event api (#43)
    * feat: update lib-* to latest version and remove mock proto
    
    * feat: add worker service

--- cmd/gen-oas/main.go
+++ cmd/gen-oas/main.go
@@ -13,7 +13,7 @@ func main() {
 }
 
 func run() int {
-	api := server.NewAPI(http.NewServeMux(), "oas-sandbox", nil)
+	api := server.NewAPI(http.NewServeMux(), "oas-sandbox", nil, nil)
 	body, err := api.OpenAPI().YAML()
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "generate OpenAPI: %v\n", err)
